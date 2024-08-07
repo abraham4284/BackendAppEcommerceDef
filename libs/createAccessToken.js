c9c592c03897a59e83a6b989f610d78c3ea4500f
Abraham <abraham4284@hotmail.com>
2024-08-07 12:43:01 -0300

    Cambiamos la configuracion de la cookies

--- libs/createAccessToken.js
+++ libs/createAccessToken.js
@@ -2,10 +2,9 @@ import { TOKEN_SECRET } from "../config.js";
 import jwt from 'jsonwebtoken';
 
 
-
 export const createAccesToken = (data = {})=>{
     try {
-        return jwt.sign(data,TOKEN_SECRET)
+        return jwt.sign(data,TOKEN_SECRET, {expiresIn: '2d'})
     } catch (error) {
         console.log("Error en la creacion del token",error)
     }
